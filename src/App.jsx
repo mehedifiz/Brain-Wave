096896d7f917cb69254fcc031d4f364383d183fe
mehedifiz <mehedi.shanto.contract@gmail.com>
2024-10-19 22:40:29 +0600

    init project

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import ButtonGradient from './assets/svg/ButtonGradient'
+import Button from './Components/Button'
+import Header from './Components/Header'
+
+const App =()=> {
+
+  return (
+    <>
+
+
+
+      <div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden'>
+     <Header></Header>
+
+      </div>
+    
+      
+      <ButtonGradient/>
+    </>
+  )
+}
+
+export default App
